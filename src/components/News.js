6d5148ce3479a46ee82ed24e979968ef0823906d
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2021-01-07 16:36:45 +0300

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,19 @@
 import useJsonFetch from'../hooks/useJsonFetch';
-
+import {useState} from'react';
 
 function News () {
-    const [data, loading, error] = useJsonFetch('http://localhost:7070/data', {});
+    const [url, setUrl] = useState('http://localhost:7070/data');
+    const [data, loading, error] = useJsonFetch(url, {});
+
 
     return (
         <>
+        <button onClick={() => setUrl('http://localhost:7070/data')}>успешное получение данных</button>
+        <button onClick={() => setUrl('http://localhost:7070/error')}>получение 500 ошибки</button>
+        <button onClick={() => setUrl('http://localhost:7070/loading')}>индикатор загрузки</button>
+        {data && data}
+        {loading && 'Загрузка'}
+        {error && 'Ошибка'}
         </>
     )
 }
